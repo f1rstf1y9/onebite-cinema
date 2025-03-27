b067434055b00dfa08c712528739074647ebcdb6
f1rstf1y9 <f1rstf1y9@naver.com>
2025-03-27 23:25:40 +0900

    글로벌 레이아웃 설정

---
+++ src/components/global-layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import style from "./global-layout.module.css";
+import Link from "next/link";
+
+export default function GlobalLayout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <header className={style.header}>
+        <Link href="/">ONEBITE CINEMA</Link>
+      </header>
+      {children}
+    </div>
+  );
+}
