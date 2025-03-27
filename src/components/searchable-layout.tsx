df31fd1b6c1c5e0b6793b204f6239248cbc92b1c
f1rstf1y9 <f1rstf1y9@naver.com>
2025-03-27 23:54:19 +0900

    검색바 로직 작성

--- src/components/searchable-layout.tsx
+++ src/components/searchable-layout.tsx
@@ -1,15 +1,45 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import style from "./searchable-layout.module.css";
+import { useRouter } from "next/router";
 export default function SearchableLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const q = router.query.q as string;
+
+  useEffect(() => {
+    setSearch(q);
+  }, []);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
     <div>
       <div>
-        <input placeholder="검색어를 입력하세요" />
-        <button>검색</button>
+        <input
+          value={search}
+          onChange={onChangeSearch}
+          onKeyDown={onKeyDown}
+          placeholder="검색어를 입력하세요"
+        />
+        <button onClick={onSubmit}>검색</button>
       </div>
       {children}
     </div>
