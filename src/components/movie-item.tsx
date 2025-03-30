16ceea92b1903a78f73b0c8dada85adfc981fbd2
f1rstf1y9 <f1rstf1y9@naver.com>
2025-03-30 17:37:17 +0900

    MovieItem 컴포넌트 구현

---
+++ src/components/movie-item.tsx
@@ -0,0 +1,20 @@
+import { MovieData } from "@/types";
+import Link from "next/link";
+
+export default function MovieItem({
+  id,
+  title,
+  subTitle,
+  description,
+  releaseDate,
+  company,
+  genres,
+  runtime,
+  posterImgUrl,
+}: MovieData) {
+  return (
+    <Link href={`/movie/${id}`}>
+      <img src={posterImgUrl} />
+    </Link>
+  );
+}
