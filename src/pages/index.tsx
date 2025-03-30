5050a068986f68959d2170c82e770e6bef7423a2
f1rstf1y9 <f1rstf1y9@naver.com>
2025-03-30 17:38:45 +0900

    인덱스 페이지 MovieItem 컴포넌트 렌더링

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
+import movies from "@/mock/movies.json";
+import MovieItem from "@/components/movie-item";
 
 export default function Home() {
   return (
-    <>
-      <h1>ONEBITE CINEMA</h1>
-    </>
+    <div>
+      <section>
+        <h3>지금 가장 추천하는 영화</h3>
+        {movies.slice(0, 3).map((movie) => (
+          <MovieItem key={movie.id} {...movie} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 영화</h3>
+        {movies.map((movie) => (
+          <MovieItem key={movie.id} {...movie} />
+        ))}
+      </section>
+    </div>
   );
 }
 
