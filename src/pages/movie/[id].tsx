7407a7d2f5bec3476663137de15d2a49d2e5f21a
f1rstf1y9 <f1rstf1y9@naver.com>
2025-03-31 22:51:04 +0900

    영화 상세 페이지 데이터 페칭

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -1,32 +1,47 @@
-import { useRouter } from "next/router";
-import movies from "@/mock/movies.json";
 import style from "./[id].module.css";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { fetchOneMovie } from "@/lib/fetch-one-movie";
 
-export default function Page() {
-  const router = useRouter();
-  const { id } = router.query;
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const id = context.params!.id;
+  const movie = await fetchOneMovie(Number(id));
+  return {
+    props: { movie },
+  };
+};
 
-  const movie = movies.find((movie) => movie.id === Number(id));
-
-  if (!movie) {
-    return <div>영화를 찾을 수 없습니다.</div>;
-  }
+export default function Page({
+  movie,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!movie) return "문제가 발생했습니다. 다시 시도하세요.";
+  const {
+    title,
+    subTitle,
+    releaseDate,
+    genres,
+    runtime,
+    company,
+    description,
+    posterImgUrl,
+  } = movie;
 
   return (
     <div className={style.container}>
       <div
         className={style.cover_img_container}
-        style={{ backgroundImage: `url('${movie.posterImgUrl}')` }}
+        style={{ backgroundImage: `url('${posterImgUrl}')` }}
       >
-        <img src={movie.posterImgUrl} />
+        <img src={posterImgUrl} />
       </div>
-      <div className={style.title}>{movie.title}</div>
+      <div className={style.title}>{title}</div>
       <div className={style.detailInfo}>
-        {movie.releaseDate} / {movie.genres.join(", ")} / {movie.runtime}
+        {releaseDate} / {genres.join(", ")} / {runtime}
       </div>
-      <div className={style.company}>{movie.company}</div>
-      <div className={style.subTitle}>{movie.subTitle}</div>
-      <div className={style.description}>{movie.description}</div>
+      <div className={style.company}>{company}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.description}>{description}</div>
     </div>
   );
 }
