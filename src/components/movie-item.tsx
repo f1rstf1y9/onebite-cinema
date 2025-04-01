97fc211cb08ddde6afccf4773a091ce8b0df40f2
f1rstf1y9 <f1rstf1y9@naver.com>
2025-04-01 16:47:01 +0900

    리팩토링

--- src/components/movie-item.tsx
+++ src/components/movie-item.tsx
@@ -2,17 +2,7 @@ import { MovieData } from "@/types";
 import Link from "next/link";
 import style from "./movie-item.module.css";
 
-export default function MovieItem({
-  id,
-  title,
-  subTitle,
-  description,
-  releaseDate,
-  company,
-  genres,
-  runtime,
-  posterImgUrl,
-}: MovieData) {
+export default function MovieItem({ id, posterImgUrl }: MovieData) {
   return (
     <Link className={style.container} href={`/movie/${id}`}>
       <img src={posterImgUrl} />
