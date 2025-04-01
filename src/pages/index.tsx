d0e69a108a1bd9a7439d945c2bd8dbea95b51acd
f1rstf1y9 <f1rstf1y9@naver.com>
2025-04-01 15:45:19 +0900

    인덱스 페이지에 SSG 방식 사전 렌더링 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,10 @@ import { ReactNode } from "react";
 import MovieItem from "@/components/movie-item";
 import style from "./index.module.css";
 import fetchMovie from "@/lib/fetch-movies";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import { fetchRandomMovies } from "@/lib/fetch-random-movies";
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const [allMovies, recoMovies] = await Promise.all([
     fetchMovie(),
     fetchRandomMovies(),
@@ -19,7 +19,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allMovies,
   recoMovies,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section className={style.recommend_section}>
